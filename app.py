56995da9ac69ddbf2e3061d83d786b568516e771
leoemn <sagarjoshi1013@gmail.com>
2023-10-09 14:20:11 +0100

    modified app.py to load ML model to answer

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
 CORS(app)
+
+# Load the model and vectorizer
+classifier = joblib.load('G:\\chat-bot\\models\\classifier.pkl')
+vectorizer = joblib.load('G:\\chat-bot\\models\\vectorizer.pkl')
 
 @app.route('/')
 def index():
@@ -13,7 +19,13 @@ def index():
 def send_message():
     data = request.json
     user_message = data.get('userMessage', '')
-    response_message = f"{user_message}"
+
+    # Vectorize the user message using the loaded vectorizer
+    user_message_vectorized = vectorizer.transform([user_message])
+
+    # Use the loaded model to generate a response
+    response_message = classifier.predict(user_message_vectorized)[0]
+
     return jsonify({'responseMessage': response_message})
 
 if __name__ == '__main__':
